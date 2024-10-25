e7bef0990aaaa00401ae9e9ca2422bfa662b32ae
Adim66 <159138289+Adim66@users.noreply.github.com>
2024-10-25 14:30:56 +0100

    Delete tp4Backend/TaskMaster_enit directory

--- tp4Backend/TaskMaster_enit/src/main/java/tp4/enit/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package tp4.enit.repository;
-
-import tp4.enit.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- tp4Backend/TaskMaster_enit/src/main/java/tp4/enit/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package tp4.enit.repository;
-
-import tp4.enit.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
